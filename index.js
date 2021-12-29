9f0ee9edff6a98aae67f186ab7f078230bded98f
Alison Melendez <alisonmelendez132@gmail.com>
2021-12-29 14:58:54 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+//const names = ["Charlie", "Samip", "Ali"];
+//const celebration = "birthday";
+const names = [];
+const celebration = "";
+const thankYou = [];
+
+function writeCards(names, celebration) {
+    for (let i = 0; i < names.length; i++) {
+        thankYou.push(`Thank you, ${names[i]}, for the wonderful ${celebration} gift!`);
+        debugger;
+    }
+    return thankYou;
+}
+
+writeCards(names, celebration);
+
+const number = "";
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
+countDown(number);
